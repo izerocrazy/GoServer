22039b49404c0a18123b1b49e404fe7170609b4a
izerocrazy <kuangzhenrong@kingsoft.com>
2014-03-22 17:34:25 +0800

    add tab code

--- src/HttpG/get.go
+++ src/HttpG/get.go
@@ -6,26 +6,66 @@ import (
         "net/url"
         "os"
         "strings"
-        "io"
         "code.google.com/p/go.net/html"
         "github.com/qiniu/iconv"
+        "time"
+        "strconv"
 )
 
-var nCount = 0
+var nStringMap = make(map[int][]string)
+var szQybhUrlMap = make(map[string]string)
+var szCompanyChenxinMap = make(map[string][]string)
 
 func main() {
-    gzgcjg := "http://www.gzgcjg.com/gzqypjtx/Login.aspx"
+    //gzgcjg := "http://www.gzgcjg.com/gzqypjtx/Login.aspx"
+    gzgcjg2 := "http://www.gzgcjg.com/gzqypjtx/common/LoginYbhnt.aspx"
+    //gzgcjg3 := "http://www.gzgcjg.com/gzqypjtx/common/LoginYllh.aspx"
     //gzzb := "http://www.gzzb.gd.cn/cms/wz/view/sccx/QyxxServlet?siteId=1"
 
-    url, err := url.Parse(gzgcjg)
-    checkError(err)
+    //ShowReader(GetHttpResp(url.String()))
+    //FilterBody(GetHttpResp(gzgcjg), false, "")
+    FilterBody(GetHttpResp(gzgcjg2), true, "div_2")
+    //FilterBody(GetHttpResp(gzgcjg3), true, "div_yllh")
 
-    //ShowReader(GetHttpBady(url.String()))
-    FilterBody(GetHttpBady(url.String()))
-    //ShowReader(PostHttpBady(url.String()))
+    //ShowReader(PostHttpResp(url4.String(), 1, nStringMap[1][1]))
+    for key, value := range nStringMap{
+        fmt.Println(key)
+        for key2, value2 := range value {
+            fmt.Println(value2, key2)
+            gzzb := "http://www.gzzb.gd.cn/cms/wz/view/sccx/QyxxServlet?siteId=1"
+            /*url4, err := url.Parse(gzzb)
+            checkError(err)*/
+            FilterBody2(PostHttpResp(gzzb, key, value2), value2)
+            time.Sleep(10 * time.Second)
+        }
+    }
+
+    for key, value := range szQybhUrlMap {
+        fmt.Println(key, value)
+        s := []string{"http://www.gzzb.gd.cn/", value}
+        szUrl := strings.Join(s, "");
+        FilterBody3(GetHttpResp(szUrl), key);
+        time.Sleep(10 * time.Second)
+    }
+
+    fmt.Println(szCompanyChenxinMap)
+    file, err := os.OpenFile("result.txt", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0777);
+    checkError(err)
+    defer file.Close()
+    for key, value := range szCompanyChenxinMap {
+        s := []string{key}
+        for _, value2 := range value {
+            s = append(s, value2)
+        }
+        szLine := strings.Join(s, "\t");
+        szLine = szLine + "\r\n"
+        file.WriteString(szLine)
+    }
+
+    //fmt.Println(nStringMap)
 }
 
-func GetHttpBady(szUrl string) (io.ReadCloser) {
+func GetHttpResp(szUrl string) (*http.Response) {
     client := &http.Client{}
 
     request, err := http.NewRequest("GET", szUrl, nil)
@@ -46,10 +86,10 @@ func GetHttpBady(szUrl string) (io.ReadCloser) {
         os.Exit(4)
     }
 
-    return response.Body
+    return response
 }
 
-func PostHttpBady(szUrl string, szQymc string) (io.ReadCloser) {
+func PostHttpResp(szUrl string, nSelTypeId int, szQymc string) (*http.Response) {
     client := &http.Client{}
     values := make(url.Values)
 
@@ -60,11 +100,18 @@ func PostHttpBady(szUrl string, szQymc string) (io.ReadCloser) {
     szGbk := cd.ConvString(szQymc)
 
     values.Set("qyxx_qymc", szGbk)
-    values.Set("qyxx_qylx", "02")
+    szSelTypeId := "0"
+    if nSelTypeId > 9 {
+        szSelTypeId = strconv.Itoa(nSelTypeId)
+    } else {
+        s := []string{"0", strconv.Itoa(nSelTypeId)}
+        szSelTypeId = strings.Join(s, "");
+    }
+    values.Set("qyxx_qylx", szSelTypeId)
 
     request, err := http.NewRequest("POST", szUrl, strings.NewReader(values.Encode()))
-    fmt.Println(values.Encode())
     checkError(err)
+    fmt.Println(values.Encode())
 
     request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
     request.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
@@ -76,7 +123,6 @@ func PostHttpBady(szUrl string, szQymc string) (io.ReadCloser) {
     resp, err := client.Do(request)
     checkError(err)
 
-    //resp.Body.Close()
     chSet := getCharset(resp)
     fmt.Printf("got charset %s\n", chSet)
     if chSet != "UTF-8" {
@@ -84,11 +130,11 @@ func PostHttpBady(szUrl string, szQymc string) (io.ReadCloser) {
         os.Exit(4)
     }
 
-    return resp.Body
+    return resp
 }
 
-func FilterDivValu(s string) bool {
-    arr := []string{"myTab_div1","myTab_div2","myTab_div3","div_yllh","div2","myTab_divRight1","myTab_divRight2","myTab_divRight3","div_zjzx"}
+func FilterDivValue(s string) bool {
+    arr := []string{"myTab_div1","myTab_div2","myTab_div3","myTab_divRight1","myTab_divRight2","myTab_divRight3","myTab_divRight4","div_zjzx"}
     for _, a := range arr {
         if s == a {
             fmt.Println(s)
@@ -99,54 +145,84 @@ func FilterDivValu(s string) bool {
     return false
 }
 
-func FilterBody(r io.ReadCloser) {
+func GetStringToInt(s string) int {
+    StrMap := map[string]int{"myTab_div1": 1, "myTab_div2": 1, "myTab_div3": 1, "myTab_divRight1": 2, "myTab_divRight2": 2, "myTab_divRight3": 2, "myTab_divRight4": 5, "div_yllh": 12, "div_2": 6, "div_zjzx": 9}
+
+    return StrMap[s]
+}
+
+func FilterBody3(resp* http.Response, szCompanyName string) {
+    r := resp.Body
+    defer r.Close()
     doc, err := html.Parse(r)
     checkError(err)
 
-    var f func(*html.Node, bool)
-    f = func(n *html.Node, bFindDiv bool) {
-        bFind1 := false
-        bFind2 := true
-        if (n.Type == html.DocumentNode || n.Type == html.ElementNode) && n.Data == "td" {
+    var f func(*html.Node, bool, bool)
+    f = func(n *html.Node, bFindDiv1 bool, bFindDiv2 bool) {
+        if (n.Type == html.DocumentNode || n.Type == html.ElementNode) && n.Data == "div" {
             for _, a := range n.Attr {
-                if a.Val == "gridview_itemStyle" {
-                    //fmt.Println(a.Key)
-                    bFind1 = true
-                } else if(a.Val == "width:242px;") {
-                    //fmt.Println(a.Key)
-                    bFind2 = true
-                }
-            }
-        }else if (n.Type == html.ElementNode && n.Data == "div") {
-            for _, a := range n.Attr {
-                if FilterDivValu(a.Val) {
-                    if bFindDiv == false {
-                        for c:= n.FirstChild; c != nil; c = c.NextSibling {
-                            fmt.Println(c.Data)
-                        }
-                    }
-
-                    bFindDiv = true;
+                if a.Val == "qylxmc" {
+                    bFindDiv1 = true
+                } else if a.Val == "yxqz" {
+                    bFindDiv2 = true
                 }
             }
         }
 
         for c:= n.FirstChild; c != nil; c = c.NextSibling {
-            if bFind1 == true && bFind2 == true && bFindDiv == true && len(c.Data) > 6 && c.Type == html.TextNode {
-                bFind1 = false
-                bFind2 = false
-                //fmt.Println(len(c.Data))
-                fmt.Println(c.Data)
-                nCount = nCount + 1
-                fmt.Println(nCount)
+            if c.Type == html.TextNode {
+                if bFindDiv1== true {
+                    cd, err := iconv.Open("utf-8", "gbk")
+                    checkError(err)
+                    defer cd.Close()
 
-                //gzzb := "http://www.gzzb.gd.cn/cms/wz/view/sccx/QyxxServlet?siteId=1"
-                //url, err := url.Parse(gzzb)
-                //checkError(err)
+                    szGbk := cd.ConvString(c.Data)
+                    szCompanyChenxinMap[szCompanyName] = append(szCompanyChenxinMap[szCompanyName], szGbk)
+                } else if bFindDiv2 == true {
+                    szCompanyChenxinMap[szCompanyName] = append(szCompanyChenxinMap[szCompanyName], c.Data)
+                }
+            }
 
-                //ShowReader(PostHttpBady(url.String(), c.Data))
+            f(c, bFindDiv1, bFindDiv2)
+        }
 
-                //os.Exit(0)
+        if bFindDiv1 == true {
+            bFindDiv1 = false
+        }
+
+        if bFindDiv2 == true {
+            bFindDiv2 = false
+        }
+    }
+
+    f(doc, false, false)
+}
+
+func FilterBody2(resp *http.Response, szCompanyName string) {
+    r := resp.Body
+    defer r.Close()
+    doc, err := html.Parse(r)
+    checkError(err)
+
+    var f func(*html.Node, bool)
+    f = func(n *html.Node, bFindDiv bool) {
+        if (n.Type == html.DocumentNode || n.Type == html.ElementNode) && n.Data == "div" {
+            for _, a := range n.Attr {
+                if a.Val == "bszn_right_table" {
+                    bFindDiv = true
+                }
+            }
+        }
+
+        for c:= n.FirstChild; c != nil; c = c.NextSibling {
+            if (bFindDiv == true && c.Data == "a") {
+                for _, a := range c.Attr {
+                    if a.Key == "href"{
+                        bFindDiv = false;
+                        szQybhUrlMap[szCompanyName] = a.Val
+                        //fmt.Println(szQybhUrlMap)
+                    }
+                }
             }
 
             f(c, bFindDiv)
@@ -157,10 +233,57 @@ func FilterBody(r io.ReadCloser) {
         }
     }
 
-    f(doc,false)
+    f(doc, false)
 }
 
-func ShowReader(r io.ReadCloser) {
+func FilterBody(resp *http.Response, bFindDiv bool, szDivName string) {
+    r := resp.Body
+    defer r.Close()
+    doc, err := html.Parse(r)
+    checkError(err)
+
+    var f func(*html.Node, bool, string)
+    f = func(n *html.Node, bFindDiv bool, szDivName string) {
+        bFind1 := false
+        if (n.Type == html.DocumentNode || n.Type == html.ElementNode) && n.Data == "td" {
+            for _, a := range n.Attr {
+                if a.Val == "gridview_itemStyle" {
+                    //fmt.Println(a.Key)
+                    bFind1 = true
+                }
+            }
+        }else if (n.Type == html.ElementNode && n.Data == "div") {
+            for _, a := range n.Attr {
+                if FilterDivValue(a.Val) {
+                    szDivName = a.Val
+                    bFindDiv = true;
+                }
+            }
+        }
+
+        for c:= n.FirstChild; c != nil; c = c.NextSibling {
+            if bFind1 == true && bFindDiv == true && len(c.Data) > 6 && c.Type == html.TextNode {
+                bFind1 = false
+
+                nDivId := GetStringToInt(szDivName)
+                nStringMap[nDivId] = append(nStringMap[nDivId], c.Data)
+            }
+
+            f(c, bFindDiv, szDivName)
+        }
+
+        if bFindDiv == true {
+            bFindDiv = false
+        }
+    }
+
+    f(doc, bFindDiv, szDivName)
+}
+
+func ShowReader(resp *http.Response) {
+    r := resp.Body
+    defer resp.Body.Close()
+
     var buf [512]byte
     reader := r
     fmt.Println("got body")
@@ -169,7 +292,15 @@ func ShowReader(r io.ReadCloser) {
         if err != nil {
             os.Exit(0)
         }
-        fmt.Print(string(buf[0:n]))
+
+        //cd, err := iconv.Open("gbk", "utf-8")
+        //checkError(err)
+        //defer cd.Close()
+
+        //szGbk := cd.ConvString(string(buf[0:n]))
+
+        //fmt.Print(szGbk)
+        fmt.Println(string(buf[0:n]))
     }
 
     os.Exit(0)
