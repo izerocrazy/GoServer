015198b2ab4148bef85bd708a9f01ec29686e717
izerocrazy_home_computer <izerocrazy@gmail.com>
2014-05-05 00:35:46 +0800

    Fix Bugs
    windows 下输入总多一个字符【暂不解决】

--- src/test/HttpG_GZ/simple.go
+++ src/test/HttpG_GZ/simple.go
@@ -12,10 +12,12 @@ func main() {
     var nBeginId int
     var nEndId int
 
-    fmt.Print("温馨提示>> : 如果你希望进行新一轮的信息选取，请在输入前删除上次的信息文件（既文件『1.txt』和『2.txt』)。\r\n")
+    fmt.Print("温馨提示>> : 如果你希望进行新一轮的信息选取，请在输入前删除上次的信息文件（文件『1.txt』和『2.txt』)。\r\n")
     fmt.Print("请输入开始企业ID（建议：网站默认第一个企业 ID 为10002）：")
     fmt.Scanf("%d", &nBeginId)
-    fmt.Print("\r\n请输入结束企业ID（建议：目前最后一个企业 ID 至少大于20000）：")
+    var szStr string
+    fmt.Scanf("%s", &szStr)
+    fmt.Print("请输入结束企业ID（建议：目前最后一个企业 ID 至少大于20000）：")
     fmt.Scanf("%d", &nEndId)
 
     szTitleLine := "企业名称\t注册资本(万元) \t2010纳税\t2011纳税\t2012纳税\t企业类型\t有效期\r\n"
