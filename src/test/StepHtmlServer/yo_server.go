d6b738f865956c4d7378a60486132e85a3e9f836
zero kuang <izerocrazy@gmail.com>
2014-08-01 13:34:41 +0800

    增加 AddFriend 的处理

--- src/test/StepHtmlServer/yo_server.go
+++ src/test/StepHtmlServer/yo_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "Base"
     "fmt"
     "net/http"
     "encoding/json"
@@ -17,12 +18,16 @@ func testFun(w http.ResponseWriter, r *http.Request){
     fmt.Println(r.URL.Query())
 }
 
+///////////////////////////////////////////////////////
+/* RegUse */
+// 使用方法：客户端请求网址 /user/reg ，务必带上 username 参数
+// 使用例子：http://localhost:8000/user/reg?username=1
 type RegUserData struct {
     Userid int `json:"username"`
 }
 
--- 思考怎么实现数据上的继承
-type RegUserStruct struct {
+// 思考怎么实现数据上的继承
+type IRegUser struct {
     Retcode int `json:"retconde"`
     Msg string `json:"msg"`
     Datetime int `json:"datetime"`
@@ -33,7 +38,7 @@ func RegUser(w http.ResponseWriter, r *http.Request){
     /* szUserName := r.URL.Query()["username"][0] */
     /* fmt.Fprintf(w, "hello, %s", szUserName) */
     
-    reg := RegUserStruct {
+    reg := IRegUser {
         Retcode: 200,
         Msg: "ok",
         Datetime: 10,
@@ -44,13 +49,46 @@ func RegUser(w http.ResponseWriter, r *http.Request){
     encode.Encode(reg) 
 }
 
+///////////////////////////////////////////////////////
+/* AddFriend */
+// 使用方法：客户端请求网址 /friend/add ，务必带上 friendname 参数
+// 使用例子：http://localhost:8000/friend/add?friendname=1
+type AddFriendData struct {
+    Id int `json:"id"`
+    Name string `json:"name"`
+}
+
+// 思考怎么实现数据上的继承
+type IAddFriend struct {
+    Retcode int `json:"retconde"`
+    Msg string `json:"msg"`
+    Datetime int `json:"datetime"`
+    Data AddFriendData `json:"data"`  
+}
+
+func AddFriend(w http.ResponseWriter, r *http.Request){
+    szUserName := r.URL.Query()["friendname"][0]
+    /* fmt.Fprintf(w, "hello, %s", szUserName) */
+    
+    reg := IAddFriend {
+        Retcode: 200,
+        Msg: "ok",
+        Datetime: 10,
+        Data: AddFriendData { Id : 100 , Name : szUserName},
+    }
+    
+    encode := json.NewEncoder(w)
+    encode.Encode(reg) 
+}
+
+
 func main() {
     HtmlServer := http.FileServer(http.Dir("."))
     http.Handle("/", HtmlServer)
     http.HandleFunc("/test", testFun)
 
     http.HandleFunc("/user/reg", RegUser)
-    //http.HandleFunc("/friend/add", AddFriend)
+    http.HandleFunc("/friend/add", AddFriend)
     //http.HandleFunc("/friend/list", GetFriendList)
     //http.HandleFunc("/yo/sendyo", SendYO)
     //http.HandleFunc("/yo/getyo", GetYO)    
@@ -58,11 +96,5 @@ func main() {
     // 缺少一个默认的 404 
 
     err := http.ListenAndServe(":8000", nil)
-    CheckErr(err)
-}
-
-func CheckErr(e error) {
-    if e != nil {
-        fmt.Println("error :", e.Error())
-    }
+    Base.CheckErr(err)
 }
