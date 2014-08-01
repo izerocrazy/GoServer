91db947a0c74f68304f731878b74c491f780b2ce
zero kuang <izerocrazy@gmail.com>
2014-08-01 14:34:32 +0800

    完成所有接口响应

--- src/test/StepHtmlServer/yo_server.go
+++ src/test/StepHtmlServer/yo_server.go
@@ -81,6 +81,106 @@ func AddFriend(w http.ResponseWriter, r *http.Request){
     encode.Encode(reg) 
 }
 
+///////////////////////////////////////////////////////
+/* GetFriendList */
+// 使用方法：客户端请求网址 /friend/list，务必带上 username 参数
+// 使用例子：http://localhost:8000/friend/list?username=1
+type User struct {
+    Name string `json:"name"`
+    Id int `json:"id"`
+}
+
+type GetFriendListData struct {
+    Count int `json:"count"`
+    Friend []User `json:"friend"`
+}
+
+// 思考怎么实现数据上的继承
+type IGetFriendList struct {
+    Retcode int `json:"retconde"`
+    Msg string `json:"msg"`
+    Datetime int `json:"datetime"`
+    Data GetFriendListData `json:"data"`  
+}
+
+func GetFriendList(w http.ResponseWriter, r *http.Request){
+    /* szUserName := r.URL.Query()["friendname"][0] */
+    /* fmt.Fprintf(w, "hello, %s", szUserName) */
+    
+    reg := IGetFriendList {
+        Retcode: 200,
+        Msg: "ok",
+        Datetime: 10,
+        Data: GetFriendListData { Count : 1 , Friend : []User{{Name : "lz", Id : 1}}},
+    }
+    
+    encode := json.NewEncoder(w)
+    encode.Encode(reg) 
+}
+
+///////////////////////////////////////////////////////
+/* SendYO */
+// 使用方法：客户端请求网址 /yo/sendyo ，务必带上 friendname 和 username 参数
+// 使用例子：http://localhost:8000/yo/sendyo?friendname=1
+// 思考怎么实现数据上的继承
+type ISendYO struct {
+    Retcode int `json:"retconde"`
+    Msg string `json:"msg"`
+    Datetime int `json:"datetime"`
+}
+
+func SendYO(w http.ResponseWriter, r *http.Request){
+    /* szUserName := r.URL.Query()["friendname"][0] */
+    /* fmt.Fprintf(w, "hello, %s", szUserName) */
+    
+    reg := ISendYO {
+        Retcode: 200,
+        Msg: "ok",
+        Datetime: 10,
+    }
+    
+    encode := json.NewEncoder(w)
+    encode.Encode(reg) 
+}
+
+///////////////////////////////////////////////////////
+/* GetYO */
+// 使用方法：客户端请求网址 /yo/getyo，务必带上 username 参数
+// 使用例子：http://localhost:8000/yo/getyo?username=1
+type YOMsg struct {
+    From string `json:"from"`
+    Msg string `json:"msg"`
+    Senddate int `json:"senddate"`
+}
+
+type GetYOData struct {
+    Count int `json:"count"`
+    Msgs []YOMsg `json:"msgs"`
+}
+
+// 思考怎么实现数据上的继承
+type IGetYO struct {
+    Retcode int `json:"retconde"`
+    Msg string `json:"msg"`
+    Datetime int `json:"datetime"`
+    Data GetYOData `json:"data"`  
+}
+
+func GetYO(w http.ResponseWriter, r *http.Request){
+    /* szUserName := r.URL.Query()["friendname"][0] */
+    /* fmt.Fprintf(w, "hello, %s", szUserName) */
+    
+    reg := IGetYO {
+        Retcode: 200,
+        Msg: "ok",
+        Datetime: 10,
+        Data: GetYOData { Count : 1 , Msgs :[]YOMsg{{From : "lz", Msg : "1", Senddate : 2}}},
+    }
+    
+    encode := json.NewEncoder(w)
+    encode.Encode(reg) 
+}
+
 
 func main() {
     HtmlServer := http.FileServer(http.Dir("."))
@@ -89,9 +189,9 @@ func main() {
 
     http.HandleFunc("/user/reg", RegUser)
     http.HandleFunc("/friend/add", AddFriend)
-    //http.HandleFunc("/friend/list", GetFriendList)
-    //http.HandleFunc("/yo/sendyo", SendYO)
-    //http.HandleFunc("/yo/getyo", GetYO)    
+    http.HandleFunc("/friend/list", GetFriendList)
+    http.HandleFunc("/yo/sendyo", SendYO)
+    http.HandleFunc("/yo/getyo", GetYO)    
 
     // 缺少一个默认的 404 
 
