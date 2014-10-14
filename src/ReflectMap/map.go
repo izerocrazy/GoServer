98fbe51de0e6090fecd78c3390214cfa0406cb99
zero kuang <izerocrazy@gmail.com>
2014-10-14 22:35:26 +0800

    为 routor 做准备，增加 Map

---
+++ src/ReflectMap/map.go
@@ -0,0 +1,57 @@
+package reflectmap
+
+import (
+	"reflect"
+)
+
+type ReflectMap struct {
+	Table map[string]reflect.Type
+}
+
+func (r *ReflectMap) Init() {
+	r.Table = make(map[string]reflect.Type)
+}
+
+/*
+函数名：注册 string 和一个 reflect.type 的关系
+
+参数：szRegName 待注册的名字, rt reflect.Type
+
+返回值：error 错误码
+
+success
+
+isexist 这个字符串已经有了对应的 reflect.type
+*/
+func (r *ReflectMap) Add(szRegName string, i interface{}) string {
+	if r.Table[szRegName] != nil {
+		return "isexist"
+	}
+
+	rt := reflect.ValueOf(i)
+	r.Table[szRegName] = rt.Type()
+	return "success"
+}
+
+/*
+函数名：传入 string，生成一个类型对应的指针
+
+参数：szRegName 注册名称
+
+返回值：error 错误码
+
+success
+
+regempty 这个字符串已经有了对应的 reflect.type
+*/
+func (r *ReflectMap) New(szRegName string) (err string, i interface{}) {
+	t := r.Table[szRegName]
+	if t != nil {
+		i = reflect.New(t).Interface()
+		err = "success"
+	} else {
+		err = "regempty"
+	}
+
+	return err, i
+}
---
+++ src/ReflectMap/map_test.go
@@ -0,0 +1,49 @@
+package reflectmap
+
+import (
+	// "fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	var r ReflectMap
+	r.Init()
+
+	var x int
+	err := r.Add("int", x)
+	if err != "success" {
+		t.Log("add int err, ", err)
+		t.FailNow()
+	}
+
+	err = r.Add("int", x)
+	if err != "isexist" {
+		t.Log("second add int err, ", err)
+		t.FailNow()
+	}
+}
+
+func TestNew(t *testing.T) {
+	var r ReflectMap
+	r.Init()
+
+	var x int
+	x = 1
+	err := r.Add("int", x)
+	if err != "success" {
+		t.Log("add int err, ", err)
+		t.FailNow()
+	}
+
+	err2, _ := r.New("int")
+	if err2 != "success" {
+		t.Log("New Int err, ", err)
+		t.FailNow()
+	}
+
+	err3, _ := r.New("int2")
+	if err3 != "regempty" {
+		t.Log("New Int2 err", err)
+		t.FailNow()
+	}
+}
